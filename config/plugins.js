fdd7f92b693d0b4413e8a0535e8dc0d431d8b8b6
Ravikumar <ravi@nfnlabs.in>
2024-08-27 11:19:03 +0530

    dashboard-url fix

--- config/plugins.js
+++ config/plugins.js
@@ -2,8 +2,6 @@ module.exports = {
   "custom-dashboard": {
     enabled: true,
     resolve: "./src/plugins/custom-dashboard",
-    appUrl: process.env.STRAPI_API_URL,
-    appApiKey: `Bearer ${process.env.STRAPI_API_TOKEN}`,
   },
   "theatre-search": {
     enabled: true,
